a5d0c35191dc756777c273a77de2c2f900a17aea
Elton Mozart <elton.mozart@gmail.com>
2024-02-17 01:21:05 +0000

    finish '/api/v1/status' endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,26 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log("result:\n", result.rows);
-  response
-    .status(200)
-    .json({ mensagem: "Alunos do curso.dev são acima da mádia." });
+  const updatedAt = new Date().toISOString();
+  const dbName = process.env.POSTGRES_DB;
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbOpenedConnections = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [dbName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion.rows[0].server_version,
+        max_connections: parseInt(dbMaxConnections.rows[0].max_connections),
+        opened_connections: dbOpenedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
